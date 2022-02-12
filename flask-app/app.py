aefa5423bf6f39cb1a01c3117e0e0186c54feefe
unknown <alyu@ucsd.edu>
2022-02-11 17:18:12 -0800

    separate cleaning and eda, basic plotly in flask

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    df = pd.read_csv('../data/processed.tsv', sep='\t')
+    
+    fig = px.scatter(df, x='yoe_total', y='tc')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('index.html', graphJSON=graphJSON)
